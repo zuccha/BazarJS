67c35ab5bdb2ce977aeff0e715075cd1d2db3d96
Amedeo Zucchetti <amedeo.zucchetti@welld.ch>
2021-11-12 11:26:50 +0100

    build: move forge config into config file

---
+++ forge.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  packagerConfig: {
+    name: 'Bazar',
+    executableName: 'electron-starter',
+    icon: 'assets/icon',
+    extraResource: ['assets'],
+  },
+  plugins: [
+    [
+      '@electron-forge/plugin-webpack',
+      {
+        mainConfig: './webpack/main.webpack.js',
+        renderer: {
+          config: './webpack/renderer.webpack.js',
+          entryPoints: [
+            {
+              html: './public/index.html',
+              js: './src/index.tsx',
+              name: 'main_window',
+              preload: {
+                js: './electron/bridge.ts',
+              },
+            },
+          ],
+        },
+      },
+    ],
+  ],
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'Bazar',
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
