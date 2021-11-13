86686df4bc6bf3f579f0124e72aa865536a0910b
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2021-11-13 11:21:16 +0100

    feat: install chakra

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,25 +1,10 @@
-import { createGlobalStyle } from 'styled-components';
+import { ChakraProvider } from '@chakra-ui/react';
 import { AppNavigation } from './AppNavigation';
 
 export function App() {
   return (
-    <>
-      <GlobalStyle />
+    <ChakraProvider>
       <AppNavigation />
-    </>
+    </ChakraProvider>
   );
 }
-
-export const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 16px;
-    color: #E1E1E6;
-  }
-`;
