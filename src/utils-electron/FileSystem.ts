f0a00cae2b2db285ddd085c61af0906b5df28674
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2021-11-15 19:48:36 +0100

    feat: add functions to filesystem utility

--- src/utils-electron/FileSystem.ts
+++ src/utils-electron/FileSystem.ts
@@ -3,25 +3,53 @@ import path from 'path';
 import { $ErrorReport, ErrorReport } from '../utils/ErrorReport';
 
 export const $FileSystem = {
+  copyFile: (sourcePath: string, targetPath: string) => {
+    try {
+      fs.copyFileSync(sourcePath, targetPath);
+    } catch (error) {
+      return $ErrorReport.make(
+        `Failed to copy file from "${sourcePath}" to "${targetPath}"`,
+      );
+    }
+  },
+
+  createDirectory: (path: string): ErrorReport | undefined => {
+    try {
+      fs.mkdirSync(path);
+    } catch {
+      return $ErrorReport.make(`Failed to create directory "${path}"`);
+    }
+  },
+
   join: path.join,
 
+  removePath: (path: string): ErrorReport | undefined => {
+    try {
+      fs.rmSync(path, { recursive: true });
+    } catch {
+      return $ErrorReport.make(`Failed to delete path "${path}"`);
+    }
+  },
+
   validateExistsDir: (path: string): ErrorReport | undefined => {
+    const errorPrefix = 'Directory does not exist';
     return !path
-      ? $ErrorReport.make('Path is empty')
+      ? $ErrorReport.make(`${errorPrefix}: path is empty`)
       : !fs.existsSync(path)
-      ? $ErrorReport.make(`Path "${path}" does not exist`)
+      ? $ErrorReport.make(`${errorPrefix}: path "${path}" does not exist`)
       : !fs.lstatSync(path).isDirectory()
-      ? $ErrorReport.make(`Path "${path}" is not a directory`)
+      ? $ErrorReport.make(`${errorPrefix}: path "${path}" is not a directory`)
       : undefined;
   },
 
   validateExistsFile: (path: string): ErrorReport | undefined => {
+    const errorPrefix = 'File does not exist';
     return !path
-      ? $ErrorReport.make('Path is empty')
+      ? $ErrorReport.make(`${errorPrefix}: path is empty`)
       : !fs.existsSync(path)
-      ? $ErrorReport.make(`Path "${path}" does not exist`)
+      ? $ErrorReport.make(`${errorPrefix}: path "${path}" does not exist`)
       : !fs.lstatSync(path).isFile()
-      ? $ErrorReport.make(`Path "${path}" is not a file`)
+      ? $ErrorReport.make(`${errorPrefix}: path "${path}" is not a file`)
       : undefined;
   },
 
@@ -29,28 +57,33 @@ export const $FileSystem = {
     path: string,
     extension: string,
   ): ErrorReport | undefined => {
+    const errorPrefix = 'File does not have extension';
     return !path
-      ? $ErrorReport.make('Path is empty')
+      ? $ErrorReport.make(`${errorPrefix}: path is empty`)
       : !path.endsWith(extension)
       ? $ErrorReport.make(
-          `Path "${path}" does not have extension "${extension}"`,
+          `${errorPrefix}: path "${path}" does not have extension "${extension}"`,
         )
       : undefined;
   },
 
   validateIsValidName: (name: string): ErrorReport | undefined => {
+    const errorPrefix = 'Name is not valid';
     return !name
-      ? $ErrorReport.make('Name is empty')
+      ? $ErrorReport.make(`${errorPrefix}: name is empty`)
       : !/^[a-zA-Z0-9_]+$/.test(name)
-      ? $ErrorReport.make(`Name "${name}" contains invalid characters`)
+      ? $ErrorReport.make(
+          `${errorPrefix}: name "${name}" contains invalid characters`,
+        )
       : undefined;
   },
 
   validateNotExists: (path: string): ErrorReport | undefined => {
+    const errorPrefix = 'Path already exists';
     return !path
-      ? $ErrorReport.make('Path is empty')
+      ? $ErrorReport.make(`${errorPrefix}: path is empty`)
       : fs.existsSync(path)
-      ? $ErrorReport.make(`Path "${path}" already exist`)
+      ? $ErrorReport.make(`${errorPrefix}: path "${path}" already exist`)
       : undefined;
   },
 };
