9f5eeae35907cfd7aae22ed7c837325dedc95a0c
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2021-11-16 00:23:53 +0100

    feat: add basic project manipulation

--- src/ui/pages/Project/Project.tsx
+++ src/ui/pages/Project/Project.tsx
@@ -1,10 +1,24 @@
-import { Center, Heading } from '@chakra-ui/react';
+import { Center, Heading, Text, VStack } from '@chakra-ui/react';
 import { ReactElement } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from '../../../store';
+import { getProjectName, setProjectName } from '../../../store/project';
+import Button from '../../../ui-atoms/input/Button';
 
 export default function Project(): ReactElement {
+  const dispatch = useDispatch<AppDispatch>();
+  const projectName = useSelector(getProjectName()) ?? '-';
+
   return (
     <Center flex={1}>
-      <Heading>Project</Heading>
+      <VStack>
+        <Heading>Project</Heading>
+        <Text>{`Name: ${projectName}`}</Text>
+        <Button
+          label='Update'
+          onClick={() => dispatch(setProjectName(projectName + '_'))}
+        />
+      </VStack>
     </Center>
   );
 }
