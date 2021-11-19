2672d09ce6f7d75cc2b18860801b93466e06580b
Amedeo Zucchetti <amedeo.zucchetti@welld.ch>
2021-11-19 08:50:21 +0100

    refactor: extract drawer component

--- src/ui/pages/Project/Sidebar/ConfigEditor.tsx
+++ src/ui/pages/Project/Sidebar/ConfigEditor.tsx
@@ -1,13 +1,4 @@
-import {
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  VStack,
-} from '@chakra-ui/react';
+import { Alert, AlertIcon, Box, VStack } from '@chakra-ui/react';
 import { ReactElement } from 'react';
 import { ProjectConfig } from '../../../../core/Project';
 import Button from '../../../../ui-atoms/input/Button';
@@ -16,6 +7,7 @@ import FormControl, {
   useFormField,
 } from '../../../../ui-atoms/input/FormControl';
 import TextInput from '../../../../ui-atoms/input/TextInput';
+import Drawer from '../../../../ui-atoms/overlay/Drawer';
 
 const { $FileSystem } = window.api;
 
@@ -31,7 +23,7 @@ export default function ConfigEditor({
   onConfirm,
 }: ConfigEditorProps): ReactElement {
   const nameField = useFormField({
-    infoMessage: 'This will be the name fo the project directory.',
+    infoMessage: 'This is the name of the project',
     initialValue: config.name,
     isRequired: true,
     label: 'Project name',
@@ -39,7 +31,7 @@ export default function ConfigEditor({
   });
 
   const authorField = useFormField({
-    infoMessage: 'Author of the project.',
+    infoMessage: 'Author of the project',
     initialValue: config.author,
     isRequired: false,
     label: 'Author',
@@ -57,43 +49,46 @@ export default function ConfigEditor({
   });
 
   return (
-    <Drawer isOpen placement='right' onClose={onCancel}>
-      <DrawerOverlay />
-      <DrawerContent>
-        <DrawerCloseButton />
-        <DrawerHeader borderBottomWidth='1px'>Edit config</DrawerHeader>
-
-        <DrawerBody>
-          <VStack>
-            <FormControl {...nameField.control}>
-              <TextInput
-                onBlur={nameField.handleBlur}
-                onChange={nameField.handleChange}
-                placeholder={nameField.control.label}
-                value={nameField.value}
-              />
-            </FormControl>
-
-            <FormControl {...authorField.control}>
-              <TextInput
-                onBlur={authorField.handleBlur}
-                onChange={authorField.handleChange}
-                placeholder={authorField.control.label}
-                value={authorField.value}
-              />
-            </FormControl>
-          </VStack>
-        </DrawerBody>
-
-        <DrawerFooter borderTopWidth='1px'>
+    <Drawer
+      buttons={
+        <>
           <Button label='Cancel' onClick={onCancel} variant='outline' mr={3} />
           <Button
             label='Save'
             onClick={form.handleSubmit}
             isDisabled={!form.isValid}
           />
-        </DrawerFooter>
-      </DrawerContent>
+        </>
+      }
+      onClose={onCancel}
+      title='Edit config'
+    >
+      <VStack flex={1}>
+        <FormControl {...nameField.control}>
+          <TextInput
+            onBlur={nameField.handleBlur}
+            onChange={nameField.handleChange}
+            placeholder={nameField.control.label}
+            value={nameField.value}
+          />
+        </FormControl>
+
+        <FormControl {...authorField.control}>
+          <TextInput
+            onBlur={authorField.handleBlur}
+            onChange={authorField.handleChange}
+            placeholder={authorField.control.label}
+            value={authorField.value}
+          />
+        </FormControl>
+
+        <Box flex={1} />
+
+        <Alert status='info'>
+          <AlertIcon />
+          Changing the project name will not change the directory name.
+        </Alert>
+      </VStack>
     </Drawer>
   );
 }
