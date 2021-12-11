e44f16fe2d93de894cca3d03a5d71ca115bba347
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2021-12-11 19:50:14 +0100

    fix: fix horizontal scrollbar style

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -1,10 +1,10 @@
 const makeScrollbarStyle = (color: string) => ({
-  borderRight: `8px solid ${color}`,
+  background: color,
 });
 
 const styles = {
   global: {
-    '::-webkit-scrollbar': { width: '14px' },
+    '::-webkit-scrollbar': { width: '10px', height: '10px' },
     '::-webkit-scrollbar-track': makeScrollbarStyle('#E5E5E5'),
     '::-webkit-scrollbar-thumb': makeScrollbarStyle('#AAAAAA'),
     '::-webkit-scrollbar-thumb:hover': makeScrollbarStyle('#888888'),
